8ea773423482715f9fb6e5e3d914c72722c3ae51
Fabio Dinis <fabiodiniseng@outlook.com>
2025-11-02 15:57:17 +0000

    atualizado

---
+++ xadrez-nivel-novato.c
@@ -0,0 +1,29 @@
+int main(){
+    
+    int t, b, r;
+
+    // mover a torre
+    printf("**** MOVIMENTO DA TORRE **** \n");
+    for (t = 0; t < 5; t++) {
+    
+        printf("Uma Casa a Direita \n"); // direção do movimento
+    }
+    // mover o bispo
+    b = 0;
+     printf("**** MOVIMENTO DO BISPO **** \n");
+     while (b < 5) {
+
+        printf("Uma Casa a Direita \n"); // direção do movimento
+        printf("Uma Casa para Cima \n"); // direção do movimento
+        b++;
+
+     }
+     printf("**** MOVIMENTO DA RAINHA **** \n");
+     r = 0;
+    do {
+        printf("Uma Casa a Esquerda \n"); // direção do movimento
+        r++;
+    } while (r < 8); 
+
+    return 0;
+}
